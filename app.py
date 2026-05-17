5d4939db574b0e4e7d955102b0bf6554880770cc
data1-Analyst <bendahmanewissam7@gmail.com>
2026-05-17 16:41:48 +0000

    mise à jour de app.py

--- app.py
+++ app.py
@@ -8,3 +8,34 @@ figure = px.pie(données, values='qte', names='region', title='quantité vendue 
 figure.write_html('ventes-par-region.html')
 
 print('ventes-par-région.html généré avec succès !')
+
+# 2. Ventes par produit 
+
+figure2 = px.bar(
+    données,
+    x='produit',
+    y='qte',
+    title='Ventes par produit (quantité)',
+    color='produit'
+)
+
+figure2.write_html('ventes-par-produit.html')
+print('ventes-par-produit.html généré avec succès !')
+
+
+# 3. Chiffre d’affaires par produit
+
+# Calcul du CA
+données['ca'] = données['qte'] * données['prix']
+
+figure3 = px.bar(
+    données,
+    x='produit',
+    y='ca',
+    title="Chiffre d'affaires par produit",
+    color='produit'
+)
+
+figure3.write_html('ca-par-produit.html')
+print('ca-par-produit.html généré avec succès !')
+
